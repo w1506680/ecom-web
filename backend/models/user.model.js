f3691e0eca62658124b61538b042c3a6b42763ba
w1506680 <69374735+w1506680@users.noreply.github.com>
2024-02-20 09:41:59 +0530

    Addeduser.model.js

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+// user.model.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
